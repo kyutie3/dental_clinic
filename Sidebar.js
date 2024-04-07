3ab39453b544f3214430023206f1d7c5a24ecb5c
kyutie3 <147722703+kyutie3@users.noreply.github.com>
2024-04-07 17:33:54 +0800

    Add files via upload

---
+++ Sidebar.js
@@ -0,0 +1,10 @@
+// Open the sidebar
+function openNav() {
+    document.getElementById("mySidebar").style.left = "0";
+  }
+  
+  // Close the sidebar
+  function closeNav() {
+    document.getElementById("mySidebar").style.left = "-250px";
+  }
+  
